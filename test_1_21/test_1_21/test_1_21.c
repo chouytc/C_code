2b0a2ec9db3a9d4a5161caf2d00480eff5204558
chouytc <chou_yin@163.com>
2023-01-21 02:03:12 +0800

    学习常量的代码示例

--- test_1_21/test_1_21/test_1_21.c
+++ test_1_21/test_1_21/test_1_21.c
@@ -41,14 +41,68 @@
 //全局变量的生命周期：程序的生命周期
 
 
+//int main()
+//{
+//	{
+//		int a = 10;
+//		printf("%d\n", a);
+//	}
+//
+//	return 0;
+//}
+
+
+//常量
+
+//#define MAX 10000
+//
+//int main()
+//{
+//	1. 字面常量
+//	3.14;
+//	10;
+//	'a';
+//	"abcdef";
+//
+//	2. const修饰的常变量
+//	const int num = 10;//num就是常变量 - 具有常属性（不能被改变的属性）
+//	num = 20;
+//	printf("num = %d\n", num);
+//	
+//	int arr[10] = { 0 };//10个元素
+//
+//	const int n = 10;//常变量本质上还是一个变量，只是具有常属性
+//	int arr2[n] = { 0 };//n是变量的，这里是不行
+//
+//	3. #define 定义的标识符常量
+//	MAX = 20000;//ERROR
+//	int n = MAX;
+//	printf("n = %d\n", n);
+//
+//	return 0;
+//}
+
+//性别
+enum Sex
+{
+	//这种枚举类型的变量的未来可能取值
+	//枚举常量
+	MALE = 3,//赋初值
+	FEMALE,
+	SECRET
+};
+
 int main()
 {
-	{
-		int a = 10;
-		printf("%d\n", a);
-	}
+	//4. 枚举常量
+	//可以一一列举的常量
+
+	enum Sex s = MALE;
+
+	printf("%d\n", MALE);
+	printf("%d\n", FEMALE);
+	printf("%d\n", SECRET);
 
 	return 0;
 }
-
 
