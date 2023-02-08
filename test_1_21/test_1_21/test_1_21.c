5a412f16f5b0e21a6684654a208fb99d80d6073a
chouytc <chou_yin@163.com>
2023-02-08 18:12:28 +0800

    初识函数&&数组

--- test_1_21/test_1_21/test_1_21.c
+++ test_1_21/test_1_21/test_1_21.c
@@ -229,3 +229,58 @@
 //
 //	return 0;
 //}
+
+//不利用函数算两数之和
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	scanf("%d%d", &num1, &num2);
+//	int sum = num1 + num2;
+//	printf("sum = %d\n", sum);
+//
+//	return 0;
+//}
+
+
+//利用函数算两数之和
+//int Add(int x, int y)
+//{
+//	int z = 0;
+//	z = x + y;
+//	return z;
+//}
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	scanf("%d%d", &num1, &num2);
+//	Add(num1, num2);
+//	int sum = Add(num1, num2);
+//	printf("sum = %d\n", sum);
+//
+//	return 0;
+//}
+
+int main()
+{
+	//int a = 1;
+	//int b = 2;
+	//int c = 3;
+	//....如果用10个变量一次存储，非常不便
+
+	//数组 - 一组相同类型的元素的集合
+	//10个整形1~10存起来
+	//数组使用下标来访问的
+
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int i = 0;
+	while (i < 10)
+	{
+		printf("%d\n", arr[i]);
+		i++;
+	}
+	//char ch[5] = { 'a','b','c' };//不完全初始化，剩余默认为0
+
+	return 0;
+}
