5a1e59dc7f3e141a33dab004d87c46212470edf0
chouytc <chou_yin@163.com>
2023-02-13 22:29:49 +0800

    初识指针代码

--- test_2_9/test_2_9/test_2_9.c
+++ test_2_9/test_2_9/test_2_9.c
@@ -207,3 +207,54 @@
 //
 //	return 0;
 //}
+
+//int main()
+//{
+//	int a = 10;//a在内存中要分配空间 - 4个字节
+//	printf("%p\n", &a);//%p 专门用来打印地址
+//
+//	int* pa = &a;//pa是用来存放地址的，在C语言中叫做指针变量
+//
+//	// * 说明 pa是指针变量
+//	// int 说明pa的对象是int类型的
+//
+//	char ch = 'w';
+//	char* pc = &ch;
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int* pa = &pa;
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 10;
+//
+//	int* pa = &a;
+//
+//	*pa = 20;// * 解引用操作  *pa就是通过pa里边的地址，找到a
+//
+//	printf("%d\n", a);
+//
+//	return 0;
+//}
+
+int main()
+//32位平台下地址是32个bit位（即4个字节）
+//64位平台下地址是64个bit位（即8个字节）
+{
+	printf("%d\n", sizeof(char*));
+	printf("%d\n", sizeof(short*));
+	printf("%d\n", sizeof(int*));
+	printf("%d\n", sizeof(long*));
+	printf("%d\n", sizeof(long long*));
+	printf("%d\n", sizeof(float*));
+	printf("%d\n", sizeof(double*));
+
+	return 0;
+}
