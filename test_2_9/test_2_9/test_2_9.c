3fb19becf15146f0ed886096d56e54fe3e14238b
chouytc <chou_yin@163.com>
2023-02-13 23:21:28 +0800

    初识结构体代码

--- test_2_9/test_2_9/test_2_9.c
+++ test_2_9/test_2_9/test_2_9.c
@@ -244,17 +244,48 @@
 //	return 0;
 //}
 
-int main()
+//int main()
 //32位平台下地址是32个bit位（即4个字节）
 //64位平台下地址是64个bit位（即8个字节）
+//{
+//	printf("%d\n", sizeof(char*));
+//	printf("%d\n", sizeof(short*));
+//	printf("%d\n", sizeof(int*));
+//	printf("%d\n", sizeof(long*));
+//	printf("%d\n", sizeof(long long*));
+//	printf("%d\n", sizeof(float*));
+//	printf("%d\n", sizeof(double*));
+//
+//	return 0;
+//}
+
+
+//结构体可以让C语言创建新的类型出来
+
+//创建一个学生
+
+struct Stu
 {
-	printf("%d\n", sizeof(char*));
-	printf("%d\n", sizeof(short*));
-	printf("%d\n", sizeof(int*));
-	printf("%d\n", sizeof(long*));
-	printf("%d\n", sizeof(long long*));
-	printf("%d\n", sizeof(float*));
-	printf("%d\n", sizeof(double*));
+	char name[20];//成员变量
+	int age;
+	double score;
+};
+
+//创建一个书的类型
+struct Book
+{
+	char name[20];
+	float price;
+	char id[30];
+};
+int main()
+{
+	struct Stu s = { "张三",20,85.5 };//结构体的创建和初始化
+	printf("1: %s %d %lf\n", s.name, s.age, s.score);// 结构体变量.成员变量
+	struct Stu* ps = &s;
+	printf("2: %s %d %lf\n", (*ps).name, (*ps).age, (*ps).score);
+
+	printf("3: %s %d %lf\n", ps->name, ps->age, ps->score);
 
 	return 0;
 }
