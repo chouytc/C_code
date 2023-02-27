beab4e956f12a1f565a0492dc8c06fec7fa14c2e
chouytc <chou_yin@163.com>
2023-02-28 00:19:43 +0800

    函数的调用实例代码

--- test_2_26/test_2_26/test.c
+++ test_2_26/test_2_26/test.c
@@ -98,35 +98,147 @@
 
 //Swap1在被调用时，实参传给形参，其实形参是实参的一份临时拷贝
 //改变形参，不能改变实参
-void Swap1(int x, int y)//传值调用
-{
-	int temp = 0;
-	temp = x;
-	x = y;
-	y = temp;
-}
+//void Swap1(int x, int y)//传值调用
+//{
+//	int temp = 0;
+//	temp = x;
+//	x = y;
+//	y = temp;
+//}
+//
+//void Swap2(int* pa, int* pb)//传址调用
+//{
+//	int z = 0;
+//	z = *pa;
+//	*pa = *pb;
+//	*pb = z;
+//}
+//
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	//写一个函数 - 交换两个整形变量的值
+//
+//	Swap1(a, b);//传值调用
+//	printf("交换前：a = %d b = %d\n", a, b);
+//	Swap2(&a, &b);//传址调用
+//	printf("交换后：a = %d b = %d\n", a, b);
+//
+//	return 0;
+//}
 
-void Swap2(int* pa, int* pb)//传址调用
+
+//1. 写一个函数可以判断一个数是不是素数。
+//#include <math.h>
+//
+//int is_prime(int n)
+//{
+//	int j = 0;
+//	for (j = 2; j <= sqrt(n); j++)
+//	{
+//		if (n % j == 0)
+//		{
+//			return 0;
+//		}
+//	}
+//
+//	return 1;
+//}
+//
+//int main()
+//{
+//	//100-200之间的素数
+//	int i = 0;
+//	int count = 0;
+//	for (i = 100; i <= 200; i++)
+//	{
+//		//判断i是否为素数
+//		if (is_prime(i) == 1)
+//		{
+//			count++;
+//			printf("%d ", i);
+//		}
+//	}
+//	printf("\ncount = %d\n", count);
+//	return 0;
+//}
+
+
+//2. 写一个函数判断一年是不是闰年。
+//一个函数不写返回类型，默认返回int类型
+//int is_leap_year(int y)
+//{
+//	if ((y % 4 == 0 && y % 100 != 0) || (y % 400 == 0))
+//	{
+//		return 1;
+//	}
+//	else
+//	{
+//		return 0;
+//	}
+//}
+//
+//int main()
+//{
+//	int year = 0;
+//	for (year = 1000; year <= 2000; year++)
+//	{
+//		if (is_leap_year(year) == 1)
+//		{
+//			printf("%d ", year);
+//		}
+//	}
+//
+//	return 0;
+//}
+
+
+//3. 写一个函数，实现一个整形有序数组的二分查找。
+//int binary_search(int *a, int k, int s)
+int binary_search(int a[], int k, int s)
 {
-	int z = 0;
-	z = *pa;
-	*pa = *pb;
-	*pb = z;
+	int left = 0;
+	int right = s - 1;
+
+	while (left <= right)
+	{
+		int mid = (left + right) / 2;
+		if (a[mid] > k)
+		{
+			right = mid - 1;
+		}
+		else if (a[mid] < k)
+		{
+			left = mid + 1;
+		}
+		else
+		{
+			return mid;
+		}
+	}
+	return -1;
 }
 
 int main()
 {
-	int a = 10;
-	int b = 20;
-	//写一个函数 - 交换两个整形变量的值
+	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int key = 7;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	//找到了，就返回找到位置的下标
+	//找不到返回-1
 
-	Swap1(a, b);
-	printf("交换前：a = %d b = %d\n", a, b);
-	Swap2(&a, &b);
-	printf("交换后：a = %d b = %d\n", a, b);
+	//数组arr传参，实际传递的不是数组的本身
+	//仅仅传过去了数组首元素的地址
+	int ret = binary_search(arr, key, sz);
+	if (-1 == ret)
+	{
+		printf("找不到\n");
+	}
+	else
+	{
+		printf("找到了，下标是：%d\n", ret);
+	}
 
 	return 0;
 }
-
-
-
