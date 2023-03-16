e1e2b25cf6a8dcbb9180168c6acce87675297dce
chouytc <chou_yin@163.com>
2023-03-16 23:44:00 +0800

    调试技巧测试代码

---
+++ 调试技巧/test_3_16/test.c
@@ -0,0 +1,116 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+//void test()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int c = a + b;
+//}
+//
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		arr[i] = i + 1;
+//	}
+//
+//	for (i = 0; i < sz; i++)
+//	{
+//		test();
+//		printf("%d\n", arr[i]);
+//	}
+//
+//	return 0;
+//}
+
+
+//void test2()
+//{
+//	printf("hehe\n");
+//}
+//
+//void test1()
+//{
+//	test2();
+//}
+//
+//void test()
+//{
+//	test1();
+//}
+//
+//int main()
+//{
+//	test();
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		arr[i] = i + 1;
+//	}
+//
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d\n", arr[i]);
+//	}
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	//1! + 2! + 3!
+//	//1  + 2  + 6 = 9
+//	//1*2*...*n
+//	int i = 0;
+//	int ret = 1;
+//	int sum = 0;
+//	int j = 0;
+//	for (j = 1; j <= n; j++)
+//	{
+//		ret = 1;
+//		for (i = 1; i <= j; i++)
+//		{
+//			ret *= i;
+//		}
+//		sum += ret;
+//	}
+//
+//	printf("%d\n", sum);
+//
+//	return 0;
+//}
+
+
+int main()
+{
+	int i = 0;
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	//0 - 9
+	for (i = 0; i <= 12; i++)
+	{
+		arr[i] = 0;
+		printf("hehe\n");
+	}
+	//代码的运行结果是什么？
+
+	return 0;
+}
