4fdae9ee0f7645a92445d9262d4b57c8c998c46d
chouytc <chou_yin@163.com>
2023-03-18 00:37:02 +0800

    调试技巧最终篇

--- 调试技巧/test_3_16/test.c
+++ 调试技巧/test_3_16/test.c
@@ -100,17 +100,149 @@
 //}
 
 
+//int main()
+//{
+//	int i = 0;
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//
+//	//0 - 9
+//	for (i = 0; i <= 12; i++)
+//	{
+//		arr[i] = 0;
+//		printf("hehe\n");
+//	}
+//	//代码的运行结果是什么？
+//
+//	return 0;
+//}
+
+
+//预防发生错误
+//通过编码的技巧，减少错误
+
+//模拟实现 strcpy
+//strcpy - 字符串拷贝
+
+//#include <string.h>
+
+//写的不好
+//void my_strcpy(char* dest, char* src)
+//{
+//	while (*src != '\0')
+//	{
+//		*dest = *src;
+//		dest++;
+//		src++;
+//	}
+//	*dest = *src;
+//}
+
+//void my_strcpy(char* dest, char* src)
+//{
+//	while (*src != '\0')
+//	{
+//		*dest++ = *src++;
+//	}
+//	*dest = *src;
+//}
+
+#include <assert.h>
+
+//把src指向的内容拷贝放进dest指向的空间中
+//从本质上讲，希望dest指向的内容被修改，src指向的内容不应该被修改
+//
+//strcpy 这个函数，其实返回的是目标空间的起始地址
+//
+//char* my_strcpy(char* dest, const char* src)
+//{
+//	assert(src != NULL);//断言
+//	assert(dest != NULL);//断言
+//	char* ret = dest;
+//	while (*dest++ = *src++)
+//	{
+//		;//hello的拷贝
+//	}
+//	return ret;//返回目标空间的起始地址
+//}
+//
+//int main()
+//{
+//	char arr1[20] = "xxxxxxxxxxx";
+//	char arr2[] = "hello";
+//
+//	//strcpy(arr1, arr2);
+//	printf("%s\n", my_strcpy(arr1, arr2));//链式访问
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	//1.
+//	//int num = 10;
+//	//int* p = &num;
+//	//*p = 20;
+//	//printf("%d\n", num);
+//
+//	//2.
+//	//const 修饰变量，这个变量就被称为常变量，不能被修改，但是本质上还是变量
+//	//
+//
+//	//const int num = 10;
+//	////num = 20;//err
+//
+//	//const int num = 10;
+//	////num = 20;//err
+//
+//	//int* const p = &num;
+//	//int n = 100;
+//	////const修饰指针变量的时候
+//	////const 如果放在*的右边，修饰的是p，表示指针变量不能被改变
+//	////      但是指针指向的内容，可以改变
+//	//*p = 20;//ok
+//	//p = &n;//err
+//
+//	//const int* p = &num;
+//	//int n = 100;
+//	////const修饰指针变量的时候
+//	////const 如果放在*的左边，修饰的是*p，表示指针指向的内容，是不能通过指针来改变的
+//	////      但是指针变量本身是可以修改的
+//	//*p = 20;
+//	//p = &n;
+//
+//	printf("%d\n", num);
+//
+//	return 0;
+//}
+
+
+//strlen 是求字符串长度的
+//my_strlen
+//1. const
+//2. assert
+
+#include <assert.h>
+//健壮性
+//鲁棒性
+//size_t - unsigned int
+size_t my_strlen(const char* str)
+{
+	assert(str != NULL);
+	size_t count = 0;
+	while (*str)
+	{
+		count++;
+		str++;
+	}
+	return count;
+}
+
 int main()
 {
-	int i = 0;
-	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
-	//0 - 9
-	for (i = 0; i <= 12; i++)
-	{
-		arr[i] = 0;
-		printf("hehe\n");
-	}
-	//代码的运行结果是什么？
+	char arr[] = "abcdef";
+
+	printf("%d\n", my_strlen(arr));
 
 	return 0;
 }
