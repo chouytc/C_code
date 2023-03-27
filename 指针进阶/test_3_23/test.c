06aae9abec316b6caea57a2dbdbf94f2a1a2420f
chouytc <chou_yin@163.com>
2023-03-27 22:56:27 +0800

    qsort与回调函数讲解代码

--- 指针进阶/test_3_23/test.c
+++ 指针进阶/test_3_23/test.c
@@ -519,95 +519,92 @@
 
 
 
-int main()
-{
-	int arr[10];
-	//数组元素的类型 - int
-	//arr数组的类型是 int[10]
-
-	return 0;
-}
-
-
+//int main()
+//{
+//	int arr[10];
+//	//数组元素的类型 - int
+//	//arr数组的类型是 int[10]
+//
+//	return 0;
+//}
 
 
-int Add(int x, int y)
-{
-	return x + y;
-}
 
-int Sub(int x, int y)
-{
-	return x - y;
-}
 
-int Mul(int x, int y)
-{
-	return x * y;
-}
+//int Add(int x, int y)
+//{
+//	return x + y;
+//}
+//
+//int Sub(int x, int y)
+//{
+//	return x - y;
+//}
+//
+//int Mul(int x, int y)
+//{
+//	return x * y;
+//}
+//
+//int Div(int x, int y)
+//{
+//	return x / y;
+//}
+//
+//void menu()
+//{
+//	printf("*****************************\n");
+//	printf("*****1. add       2. sub*****\n");
+//	printf("*****3. mul       4. div*****\n");
+//	printf("******      0. exit     *****\n");
+//	printf("*****************************\n");
+//}
+//
+//void Calc(int(*pf)(int, int))
+//{
+//	int x = 0;
+//	int y = 0;
+//	int ret = 0;
+//	printf("请输入2个操作数:>");
+//	scanf("%d %d", &x, &y);
+//	ret = pf(x, y);
+//	printf("ret = %d\n", ret);
+//}
+//
+//int main()
+//{
+//	//计算器 - 计算整形变量的加、减、乘、除
+//	int input = 0;
+//	do
+//	{
+//		menu();
+//		printf("请选择:>");
+//		scanf("%d", &input);
+//		switch (input)
+//		{
+//		case 1:
+//			Calc(Add);
+//			break;
+//		case 2:
+//			Calc(Sub);
+//			break;
+//		case 3:
+//			Calc(Mul);
+//			break;
+//		case 4:
+//			Calc(Div);
+//			break;
+//		case 0:
+//			printf("退出程序\n");
+//			break;
+//		default:
+//			printf("选择错误，重新选择\n");
+//			break;
+//		}
+//
+//	} while (input);
+//
+//	return 0;
+//}
 
-int Div(int x, int y)
-{
-	return x / y;
-}
 
-void menu()
-{
-	printf("*****************************\n");
-	printf("*****1. add       2. sub*****\n");
-	printf("*****3. mul       4. div*****\n");
-	printf("******      0. exit     *****\n");
-	printf("*****************************\n");
-}
-
-int main()
-{
-	//计算器 - 计算整形变量的加、减、乘、除
-	int input = 0;
-	do
-	{
-		int x = 0;
-		int y = 0;
-		int ret = 0;
-		menu();
-		printf("请选择:>");
-		scanf("%d", &input);
-
-		switch (input)
-		{
-		case 1:
-			printf("请输入两个操作数:>");
-			scanf("%d %d", &x, &y);
-			ret = Add(x, y);
-			printf("ret = %d\n", ret);
-			break;
-		case 2:
-			printf("请输入两个操作数:>");
-			scanf("%d %d", &x, &y);
-			ret = Sub(x, y);
-			printf("ret = %d\n", ret);
-			break;
-		case 3:
-			printf("请输入两个操作数:>");
-			scanf("%d %d", &x, &y);
-			ret = Mul(x, y);
-			printf("ret = %d\n", ret);
-			break;
-		case 4:
-			printf("请输入两个操作数:>");
-			scanf("%d %d", &x, &y);
-			ret = Div(x, y);
-			printf("ret = %d\n", ret);
-			break;
-		case 0:
-			printf("退出程序\n");
-			break;
-		default:
-			printf("选择错误，重新选择\n");
-			break;
-		}
-
-	} while (input);
-
-	return 0;
-}
